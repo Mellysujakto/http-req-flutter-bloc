8b81c2fc9e0a51c45499c013558901c85423e89c
mellysujakto <msujakto@gmail.com>
2020-03-15 23:01:55 +0700

    [TEST] customer service test

--- test/bloc_test/customer_bloc_test.dart
+++ test/bloc_test/customer_bloc_test.dart
@@ -10,21 +10,22 @@ class MockClient extends Mock with http.Client {}
 void main() {
   group("Customer Bloc Test", () {
     blocTest("Initial Customer", build: () async => CustomerBloc(), expect: []);
+   
     blocTest("to_get event",
         build: () async => CustomerBloc(),
-        act: (bloc) async => {bloc.add(CustomerEvent.to_get)},
+        act: (bloc) async => {bloc.add(Get("1"))},
         expect: [Customer(id: "1", name: "GeorgeBluth")]);
     blocTest("to_post event",
         build: () async => CustomerBloc(),
-        act: (bloc) async => {bloc.add(CustomerEvent.to_post)},
+        act: (bloc) async => {bloc.add(Post(name: "Jacto", job: "Dev"))},
         expect: [Customer(id: "4", name: "Jacto")]);
     blocTest("to_put event",
         build: () async => CustomerBloc(),
-        act: (bloc) async => {bloc.add(CustomerEvent.to_put)},
+        act: (bloc) async => {bloc.add( Put(firstName: "morpheus", lastName: "zion resident"))},
         expect: [Customer(id: "3", name: "morpheus")]);
     blocTest("to_delete event",
         build: () async => CustomerBloc(),
-        act: (bloc) async => {bloc.add(CustomerEvent.to_delete)},
+        act: (bloc) async => {bloc.add(Delete())},
         expect: [Customer(id: "2", name: "JanetWeaver")]);
   });
 }
